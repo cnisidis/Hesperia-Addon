b4a997ba5825e0f0a1971a0a3a6266bc11e91058
cnisidis <makeithappen@nisidis.com>
2023-08-06 14:03:50 +0300

    start from scratch

--- __init__.py
+++ __init__.py
@@ -1,27 +1,29 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 bl_info = {
     "name" : "Hesperia",
-    "author": "cnisidis http://nisidis.com",
-    "description" : "UDP bridge for vvvv and Blender - Preview Engine for Hesperos",
-    "blender" : (3, 00, 0),
+    "author" : "CNISIDIS",
+    "description" : "",
+    "blender" : (2, 80, 0),
     "version" : (0, 0, 1),
-    "location" : "View3D",
-     "warning":"WIP - ",
-    "support": "TESTING",
-    
-    "category": "Tools"
+    "location" : "",
+    "warning" : "",
+    "category" : "Generic"
 }
 
-import bpy
-#from . OP import Hesperia_OT_UDPOperator, Hesperia_OT_ExporterOperator
-
-classes = ()
-register_classes, unregister_classes = bpy.utils.register_classes_factory(classes)
-
-
 def register():
-    register_classes()
-    #bpy.types.Scene.BMD =  bpy.props.PointerProperty(type=BMDProperties)
+    ...
 
 def unregister():
-    unregister_classes()
-    #del bpy.types.Scene.BMD
+    ...
